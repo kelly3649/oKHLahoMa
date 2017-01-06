42892ead9ab8dfa3ca2f9e2c90815a302bf98821
Daniel Monteagudo <dmonte3210@gmail.com>
2017-01-05 21:40:15 -0500

    FIXED IT GUYS. Haley and edmond need to look at this

--- cloudinary.py
+++ cloudinary.py
@@ -9,8 +9,11 @@ def home():
         return render_template("nootnoot.html")
     else:
         things = { "file" : request.form["sneaky"], "upload_preset" : "bf17cjwp" }
-        uplode = req.post("https://api.cloudinary.com/v1_1/dhan3kbrs/auto/upload", params=things)
-        return uplode.text
+        uplode = req.post("http://api.cloudinary.com/v1_1/dhan3kbrs/auto/upload", data=things)
+        response = uplode.json()
+        photo_name = response["public_id"]
+        url = response["secure_url"]
+        return "<h1>PHOTO ID: %s</h1><img src='%s'></img><h5>%s</h5>" % (photo_name, url, url)
 
 if __name__ == "__main__":
     app.run()
