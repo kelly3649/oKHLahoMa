7c226c0ef83b3201cd73f970af88bcfcbdb6bea3
Haley Zeng <hzeng@stuy.edu>
2017-01-21 17:08:03 -0500

    upload page edits

--- static/js/postify.js
+++ static/js/postify.js
@@ -5,7 +5,6 @@ filenoodle.onchange = function() {
     loadPhoto();
     labelnoodle.innerHTML = this.value.substr(12);
     filethings = document.getElementById("file-things");
-
 };
 
 var loadPhoto = function() {
@@ -14,10 +13,18 @@ var loadPhoto = function() {
     var reader = new FileReader();
     reader.onload = function(imgfile) {
 	document.getElementById("lul").value = imgfile.target.result;
+
+	var caption = document.createElement("INPUT");
+	caption.setAttribute("type", "text");
+	caption.setAttribute("placeholder", "caption");
+
 	var submitButton = document.createElement("INPUT");
 	submitButton.setAttribute("type", "submit");
+	submitButton.setAttribute("value", "upload");
 	submitButton.className += "btn btn-default";
+
 	if (formnoodle.childElementCount < 3) {
+	    formnoodle.appendChild(caption);
 	    formnoodle.appendChild(submitButton);
 	}
 	document.getElementById("thatimage").setAttribute("src", document.getElementById("lul").value);
